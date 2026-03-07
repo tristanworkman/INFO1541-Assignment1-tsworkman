b15963d7022bda18530eaa69fd2d2ba042101bb6
tristanworkman <sungjinworkman@gmail.com>
2026-03-07 16:27:31 -0600

    I added Test Code to the CommissionEmployeeTest file.

--- src/test/java/CommissionEmployeeTest.java
+++ src/test/java/CommissionEmployeeTest.java
@@ -1,20 +1,41 @@
+import EmployeeObjects.CommissionEmployee;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
 
 class CommissionEmployeeTest
 {
+    CommissionEmployee employee = new CommissionEmployee("Clint", "Barton", 6847, "Sales", "Customer Representative", .0265);
 
     @Test
     void increaseSales()
     {
-    }
 
-    @Test
-    void annualRaise()
-    {
+        employee.increaseSales(100);
+        assertEquals(100, employee.getSales());
+        employee.increaseSales(10);
+        assertEquals(110, employee.getSales());
+        employee.increaseSales(-10); //will not do anything because it is a negative number.
+        assertEquals(110, employee.getSales());
+        employee.increaseSales(-100); //will not do anything because it is a negative number.
+        assertEquals(110, employee.getSales());
+
     }
 
     @Test
     void holidayBonus()
     {
+        double bonus = employee.holidayBonus();
+
+        assertEquals(0, bonus);
+    }
+
+    @Test
+    void annualRaise()
+    {
+        employee.annualRaise();
+        employee.annualRaise();
+
+        assertEquals(0.0305, employee.getRate());
     }
 }
