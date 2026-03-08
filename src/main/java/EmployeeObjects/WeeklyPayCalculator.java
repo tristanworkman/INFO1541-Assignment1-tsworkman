887ac4c822b6a0c10de8203985f0c942ee61d3b2
tristanworkman <sungjinworkman@gmail.com>
2026-03-08 01:05:57 -0600

    I created the annotations that were instructed to make.

---
+++ src/main/java/EmployeeObjects/WeeklyPayCalculator.java
@@ -0,0 +1,9 @@
+package EmployeeObjects;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WeeklyPayCalculator
+{
+}
