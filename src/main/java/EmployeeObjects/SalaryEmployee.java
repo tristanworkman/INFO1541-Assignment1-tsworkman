7e7fc34144c21b8c55d198cb31e8f3adcdc1fe5f
tristanworkman <sungjinworkman@gmail.com>
2026-03-07 18:09:21 -0600

    I updated holiday bonus to return a value rounded to the second decimal

--- src/main/java/EmployeeObjects/SalaryEmployee.java
+++ src/main/java/EmployeeObjects/SalaryEmployee.java
@@ -63,7 +63,7 @@ public final class SalaryEmployee extends Employee {
     @Override
     public double holidayBonus()
     {
-        return salary * .03365;
+        return (double) Math.round(salary * .03365 * 100) / 100; //this rounds to the second decimal.
     }
 
     /**
