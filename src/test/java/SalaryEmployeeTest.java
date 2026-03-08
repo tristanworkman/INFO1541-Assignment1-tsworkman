331495829ec73cfe40439de687c353e5de4a864a
tristanworkman <sungjinworkman@gmail.com>
2026-03-07 18:08:42 -0600

    I added code to test the values of a SalaryEmployee object.

--- src/test/java/SalaryEmployeeTest.java
+++ src/test/java/SalaryEmployeeTest.java
@@ -1,15 +1,26 @@
+import EmployeeObjects.SalaryEmployee;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class SalaryEmployeeTest
 {
+    SalaryEmployee employee = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
+
 
     @Test
     void calculateWeeklyPay()
     {
+        double pay = employee.calculateWeeklyPay();
+
+        assertEquals(1237.02, pay);
     }
 
     @Test
     void holidayBonus()
     {
+        double bonus = employee.holidayBonus();
+
+        assertEquals(2164.54, bonus);
     }
 }
